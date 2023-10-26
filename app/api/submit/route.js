6f0068d541102a46a8e10b77f45eadbed9bfc8e2
Brien Austin <108070853+Brien-Austin@users.noreply.github.com>
2023-10-26 14:58:45 +0530

    updated post route changes

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   return NextResponse.json({
-    message: "Welcome to the survey !!",
+    message: "Vanakam da maapla !!",
   });
 }
 
